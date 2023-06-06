bd8bbdf407a4367e4624c3af835f0ea1abfeba69
muhannadmahdi <135234347+muhannadmahdi@users.noreply.github.com>
2023-06-06 18:12:20 +0300

    Add files via upload

---
+++ exercise_2.3/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+const Header = ({ name }) => {
+  return <h2>{name}</h2>;
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return (
+    <p>
+      <strong>Total exercises: {totalExercises}</strong>
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
