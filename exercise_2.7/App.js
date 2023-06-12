e89a8828b2a0ad0bc993d84f1079f75fb2272020
muhannadmahdi <135234347+muhannadmahdi@users.noreply.github.com>
2023-06-12 13:09:05 +0300

    Add files via upload

---
+++ exercise_2.7/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    const personExists = persons.some((person) => person.name === newName);
+
+    if (personExists) {
+      alert(`${newName} is already added to the phonebook`);
+      setNewName('');
+      return;
+    }
+
+    const personObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName('');
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, index) => (
+        <div key={index}>{person.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
